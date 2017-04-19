3727db47b7b04b0f2d0890fc0b8c6fb0c7ef543a
r7kamura <r7kamura@gmail.com>
2017-04-20 02:38:44 +0900

    Treat 401 as healthy

--- terraform/modules/mastodon/aws_alb_target_group.tf
+++ terraform/modules/mastodon/aws_alb_target_group.tf
@@ -5,6 +5,15 @@ resource "aws_alb_target_group" "mastodon_node_streaming" {
   vpc_id               = "${aws_vpc.mastodon.id}"
   deregistration_delay = 10
 
+  health_check {
+    healthy_threshold   = 2
+    interval            = 60
+    matcher             = "401"
+    path                = "/"
+    timeout             = 10
+    unhealthy_threshold = 2
+  }
+
   stickiness {
     type = "lb_cookie"
   }
