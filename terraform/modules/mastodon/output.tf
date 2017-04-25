73162540a05fe0905e6d3e77e82a4bd8c33e008e
r7kamura <r7kamura@gmail.com>
2017-04-25 21:33:41 +0900

    Output ALB info for Route53

--- terraform/modules/mastodon/output.tf
+++ terraform/modules/mastodon/output.tf
@@ -5,3 +5,11 @@ output "aws_cloudfront_distribution_mastodon_domain_name" {
 output "aws_cloudfront_distribution_mastodon_hosted_zone_id" {
   value = "${aws_cloudfront_distribution.mastodon.hosted_zone_id}"
 }
+
+output "aws_alb_mastodon_dns_name" {
+  value = "${aws_alb.mastodon.dns_name}"
+}
+
+output "aws_alb_mastodon_zone_id" {
+  value = "${aws_alb.mastodon.zone_id}"
+}
