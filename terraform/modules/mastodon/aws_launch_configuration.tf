46d8a90578d76080419941f9d6750bf727e9fdc2
r7kamura <r7kamura@gmail.com>
2017-05-10 08:22:56 +0900

    Use name_prefix because LC that is being used can't be destroyed

--- terraform/modules/mastodon/aws_launch_configuration.tf
+++ terraform/modules/mastodon/aws_launch_configuration.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "mastodon" {
   iam_instance_profile        = "${aws_iam_instance_profile.mastodon.id}"
   image_id                    = "${lookup(var.aws_ecs_optimized_ami_ids, data.aws_region.current.name)}"
   instance_type               = "${var.aws_launch_configuration_mastodon_instance_type}"
-  name                        = "mastodon"
+  name_prefix                 = "mastodon-"
   security_groups             = ["${aws_security_group.mastodon_web.id}"]
   user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.mastodon.name}' >> /etc/ecs/ecs.config"
 }
