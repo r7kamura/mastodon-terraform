0827a2702d0087fbca0d640eb6261433e55b7784
r7kamura <r7kamura@gmail.com>
2017-05-15 18:00:41 +0900

    Fix wrong variable on data.tf

--- terraform/modules/mastodon/data.tf
+++ terraform/modules/mastodon/data.tf
@@ -176,7 +176,7 @@ data "template_file" "mastodon_environment_variables_rails" {
     mastodon_prepared_statements         = "${var.mastodon_prepared_statements}"
     mastodon_redis_host                  = "${aws_elasticache_cluster.mastodon.cache_nodes.0.address}"
     mastodon_redis_port                  = "${aws_elasticache_cluster.mastodon.cache_nodes.0.port}"
-    mastodon_s3_bucket                   = "${var.aws_s3_bucket.mastodon.id}"
+    mastodon_s3_bucket                   = "${var.aws_s3_bucket_name}"
     mastodon_s3_cloudfront_host          = "${var.mastodon_s3_cloudfront_host}"
     mastodon_s3_region                   = "${aws_region.current.name}"
     mastodon_secret_key_base             = "${var.mastodon_secret_key_base}"
