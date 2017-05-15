d69d4c731b4a88d188531c0e567c9d6b15d4ebdb
r7kamura <r7kamura@gmail.com>
2017-05-15 18:59:51 +0900

    Fix wrong IAM Role use

--- terraform/modules/mastodon/aws_iam_role.tf
+++ terraform/modules/mastodon/aws_iam_role.tf
@@ -46,14 +46,11 @@ resource "aws_iam_role" "mastodon_rails" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-        "s3:DeleteObject",
-        "s3:PutObject"
-      ],
+      "Action": "sts:AssumeRole",
       "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::${var.aws_s3_bucket_name}/*"
-      ]
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
     }
   ]
 }
---
+++ terraform/modules/mastodon/aws_iam_role_policy.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role_policy" "mastodon_rails" {
+  name = "${var.aws_resource_base_name}_rails"
+
+  policy = <<-JSON
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "s3:DeleteObject",
+          "s3:PutObject"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+          "arn:aws:s3:::${var.aws_s3_bucket_name}/*"
+        ]
+      }
+    ]
+  }
+  JSON
+
+  role = "${aws_iam_role.mastodon_rails.id}"
+}
