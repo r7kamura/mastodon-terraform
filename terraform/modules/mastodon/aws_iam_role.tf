b9d39cbc24dd96ec0b4948ae81afb5abd328a89f
r7kamura <r7kamura@gmail.com>
2017-05-15 19:18:14 +0900

    Refactor JSON heredoc indent

--- terraform/modules/mastodon/aws_iam_role.tf
+++ terraform/modules/mastodon/aws_iam_role.tf
@@ -2,19 +2,19 @@ resource "aws_iam_role" "mastodon_ec2" {
   name = "${var.aws_resource_base_name}_ec2"
 
   assume_role_policy = <<-JSON
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
   JSON
 }
 
@@ -22,19 +22,19 @@ resource "aws_iam_role" "mastodon_ecs" {
   name = "${var.aws_resource_base_name}_ecs"
 
   assume_role_policy = <<-JSON
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
   JSON
 }
 
@@ -42,17 +42,17 @@ resource "aws_iam_role" "mastodon_rails" {
   name = "${var.aws_resource_base_name}_rails"
 
   assume_role_policy = <<-JSON
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        }
       }
-    }
-  ]
-}
+    ]
+  }
   JSON
 }
