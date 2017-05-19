a1f99cbf641e676d52d12c102dae8b7060abde9c
r7kamura <r7kamura@gmail.com>
2017-05-19 20:13:41 +0900

    Output file cloudfront distribution info

--- terraform/modules/mastodon/output.tf
+++ terraform/modules/mastodon/output.tf
@@ -2,6 +2,14 @@ output "aws_cloudfront_distribution_mastodon_domain_name" {
   value = "${aws_cloudfront_distribution.mastodon.domain_name}"
 }
 
+output "aws_cloudfront_distribution_mastodon_file_domain_name" {
+  value = "${aws_cloudfront_distribution.mastodon_file.domain_name}"
+}
+
+output "aws_cloudfront_distribution_mastodon_file_hosted_zone_id" {
+  value = "${aws_cloudfront_distribution.mastodon_file.hosted_zone_id}"
+}
+
 output "aws_cloudfront_distribution_mastodon_hosted_zone_id" {
   value = "${aws_cloudfront_distribution.mastodon.hosted_zone_id}"
 }
