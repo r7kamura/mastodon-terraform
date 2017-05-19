6525cc2190ea8da5c65a4047489329d538f3d90c
r7kamura <r7kamura@gmail.com>
2017-05-19 16:13:29 +0900

    Locate CloudFront distribution in front of S3

--- terraform/modules/mastodon/aws_cloudfront_distribution.tf
+++ terraform/modules/mastodon/aws_cloudfront_distribution.tf
@@ -79,3 +79,49 @@ resource "aws_cloudfront_distribution" "mastodon" {
     ssl_support_method             = "sni-only"
   }
 }
+
+resource "aws_cloudfront_distribution" "mastodon_file" {
+  aliases = ["${var.mastodon_s3_cloudfront_host}"]
+  comment = "${var.aws_resource_base_name}"
+  enabled = true
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    default_ttl     = 86400
+
+    forwarded_values {
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin"]
+
+      query_string = false
+    }
+
+    max_ttl                = 86400
+    min_ttl                = 0
+    target_origin_id       = "mastodon_s3"
+    viewer_protocol_policy = "${var.aws_acm_certificate_arn == "" ? "allow-all" : "redirect-to-https"}"
+  }
+
+  origin {
+    domain_name = "${aws_s3_bucket.mastodon.bucket_domain_name}"
+    origin_id   = "mastodon_s3"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = "${var.aws_acm_certificate_arn}"
+    cloudfront_default_certificate = "${var.aws_acm_certificate_arn == "" ? "1" : "0"}"
+    minimum_protocol_version       = "TLSv1"
+    ssl_support_method             = "sni-only"
+  }
+}
